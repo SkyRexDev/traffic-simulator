bee844e1e7fb00cf2a99e966f07c0352fd1d9aa7
JoseMV86 <soy.josemv@gmail.com>
2024-01-24 19:56:05 +0100

    Cosas mías
    NO MEZCLAR CON MAIN

--- ina.vehicle.navigation.components/src/ina/vehicle/navigation/components/SmartCarClient.java
+++ ina.vehicle.navigation.components/src/ina/vehicle/navigation/components/SmartCarClient.java
@@ -1,11 +1,11 @@
 package ina.vehicle.navigation.components;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.mqttv5.client.IMqttDeliveryToken;
+import org.eclipse.paho.mqttv5.client.MqttCallback;
+import org.eclipse.paho.mqttv5.client.MqttClient;
+import org.eclipse.paho.mqttv5.common.MqttException;
+import org.eclipse.paho.mqttv5.common.MqttMessage;
 
 import ina.vehicle.navigation.utils.MySimpleLogger;
 
